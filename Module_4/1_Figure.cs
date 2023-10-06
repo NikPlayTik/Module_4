0359e722848083cd38d0ac7a7e9a7e81859fffd5
Nikita Anishenco <tanishenco91@gmail.com>
2023-10-06 10:52:38 +0300

    В 1 добавлен класс Прямоугольник и его логика

--- Module_4/1_Figure.cs
+++ Module_4/1_Figure.cs
@@ -6,5 +6,30 @@ using System.Threading.Tasks;
 
 namespace Module_4
 {
+    interface IFigure
+    {
+        double CalculateArea();
+        double CalculatePerimetr();
+    }
 
+    public class Rectangle : IFigure
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double CalculateArea()
+        {
+            return width * length;
+        }
+        public double CalculatePerimetr() 
+        {
+            return 2 * (width + length);
+        }
+    }
 }
